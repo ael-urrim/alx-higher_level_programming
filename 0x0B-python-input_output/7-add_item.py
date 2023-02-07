d8cd36dc6d8d9045243c15fa70aacb532df38a47
Ifedolapo Samuel Akindahunsi <80631962+ael-urrim@users.noreply.github.com>
2023-02-07 21:24:07 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""script to load, add and save"""
+"""
+Adds all arguments to a JSON file.
+"""
+
 from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json = __import__('7-save_to_json_file').save_to_json_file
+load_from_json = __import__('8-load_from_json_file').load_from_json_file
 
-
-filename = 'add_item.json'
-my_list = []
-try:
-    my_list = load_from_json_file(filename)
-except Exception:
-    save_to_json_file(my_list, filename)
-
-arg_len = len(argv)
-
-if arg_len > 1:
-    for i in range(1, arg_len):
-        my_list.append(argv[i])
-
-    save_to_json_file(my_list, filename)
+if __name__ == "__main__":
+    try:
+        lst = load_from_json("add_item.json")
+    except:
+        pass
+        lst = []
+    for av in argv[1:]:
+        lst.append(av)
+    save_to_json(lst, "add_item.json")
